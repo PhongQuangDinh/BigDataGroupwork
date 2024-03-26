6d9923bcb2862ed12d919edf84a4df6cd1457331
Võ Cao Trí <trivo.131003@gmail.com>
2024-03-26 20:56:46 +0700

    update

--- Lab/Lab2/KHMT1-06/src/main/java/org/example/KHMT1_06.java
+++ Lab/Lab2/KHMT1-06/src/main/java/org/example/KHMT1_06.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Set;
+import java.util.HashSet;
 import java.io.IOException;
 import java.util.StringTokenizer;
 import org.apache.hadoop.conf.Configuration;
@@ -12,7 +16,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
+import org.apache.hadoop.filecache.DistributedCache;
 
 public class KHMT1_06 {
     public static class TokenizerMapper
@@ -21,12 +25,37 @@ public class KHMT1_06 {
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
 
+        private HashSet<String> stopWords = new HashSet<String>();
+
+        protected void setup(Context context) throws IOException, InterruptedException {
+            Configuration conf = context.getConfiguration();
+            Path[] stopWordsFiles = DistributedCache.getLocalCacheFiles(conf);
+            if (stopWordsFiles != null && stopWordsFiles.length > 0) {
+                for (Path stopWordsFile : stopWordsFiles) {
+                    try (BufferedReader br = new BufferedReader(new FileReader(stopWordsFile.toString()))) {
+                        String line;
+                        while ((line = br.readLine()) != null) {
+                            stopWords.add(line.trim());
+                        }
+                    }
+                }
+            }
+        }
+
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
+/*            StringTokenizer itr = new StringTokenizer(value.toString());
             while (itr.hasMoreTokens()) {
                 word.set(itr.nextToken());
                 context.write(word, one);
+            }*/
+            StringTokenizer itr = new StringTokenizer(value.toString().toLowerCase());
+            while (itr.hasMoreTokens()) {
+                String token = itr.nextToken();
+                if (!stopWords.contains(token)) {
+                    word.set(token);
+                    context.write(word, one);
+                }
             }
         }
     }
@@ -55,14 +84,18 @@ public class KHMT1_06 {
             fs.delete(new Path(args[1]),true);
         }
         Job job = Job.getInstance(conf, "word count");
+        job.addCacheFile(new Path("/stopwords/stopwords.txt").toUri());
         job.setJarByClass(KHMT1_06.class);
         job.setMapperClass(TokenizerMapper.class);
         job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        for (int i = 0; i < args.length - 1; i++) {
+            FileInputFormat.addInputPath(job, new Path(args[i]));
+        }
+
+        FileOutputFormat.setOutputPath(job, new Path(args[args.length - 1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
